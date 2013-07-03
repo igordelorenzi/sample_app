d9dd6e500f5fabb57dbe219fce4e1265d58eeef2
igordla <igordelorenzi@gmail.com>
2013-07-03 10:30:06 -0300

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 SampleApp::Application.config.secret_token = 'ac7d8960eb6f8f79da026e5e0a15046c4352c21a6af8885a5081720e04596b0d6edf27c06d35b31f1e7933b6228e83f9aa37bfa629d9e47cb9a2720f05f2b99b'
+SampleApp::Application.config.secret_key_base = '18b5c6444ea667f9516e525fb9f37010abc6dc1956d9fbdf9eee62b386ef311a4ca007b36213a9ef466b4c8f371752919866983c3cf8be10e78b0b6e80314dd3'
